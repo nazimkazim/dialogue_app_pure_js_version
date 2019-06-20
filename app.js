e67c128d99d6402cd1ec1805263a161ecde9062a
Your Name <nazik_111@mail.ru>
2019-06-20 18:01:57 +0600

    add change audio files' names and add prompts

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const lines = {
-  line1: {
-    text: `Как дела`,
-    audio: 'audio/как_дела.mp3'
+  speaker1_1: {
+    text: `как дела`,
+    audio: 'audio/аудио1.mp3',
+    prompt: ''
   },
-  line2: {
-    text: 'У меня все хорошо, а как ты?',
-    audio: 'audio/у_меня_все_хорошо_а_как_ты.mp3'
+  speaker2_1: {
+    text: 'у меня все хорошо, а как ты?',
+    audio: 'audio/аудио2.mp3',
+    prompt: 'Say that you are OK and ask how is your partner doing'
+  },
+  speaker1_2: {
+    text: 'у меня тоже неплохо, кстати а что ты делаешь в субботу',
+    audio: 'audio/аудио3.mp3',
+    prompt: ''
+  },
+  speaker2_2: {
+    text: 'в субботу у меня футбол, а что?',
+    audio: 'audio/аудио4.mp3',
+    prompt:
+      'tell him that you have football on Saturday, and ask him why he is asking you (а что? = and what?)'
   }
 };
 var cont = document.getElementById('cont');
@@ -18,6 +31,14 @@ for (let key in lines) {
   sound.src = `${lines[key].audio}`;
   sound.type = 'audio/mpeg';
   cont.appendChild(sound);
+
+  if (lines[key].prompt !== '') {
+    var promptEl = document.createElement('p');
+    promptEl.id = 'prompt';
+    promptNode = document.createTextNode(lines[key].prompt);
+    promptEl.appendChild(promptNode);
+    cont.appendChild(promptEl);
+  }
 
   var textEl = document.createElement('p');
   textEl.id = 'text';
