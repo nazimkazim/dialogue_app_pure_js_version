b01cc648605986e26f3a041b4db3a297488dcc69
Your Name <nazik_111@mail.ru>
2019-06-20 17:22:01 +0600

    add text

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const lines = {
   line1: {
-    text: `<p>Как дела</p>`,
+    text: `Как дела`,
     audio: 'audio/как_дела.mp3'
   },
   line2: {
-    text: '<p>У меня все хорошо, а как ты?</p>',
+    text: 'У меня все хорошо, а как ты?',
     audio: 'audio/у_меня_все_хорошо_а_как_ты.mp3'
   }
 };
-var cont = document.getElementsByClassName('container');
+var cont = document.getElementById('cont');
 console.log(cont);
 
 for (let key in lines) {
-  console.log(lines[key].audio);
-  console.log(lines[key].text);
   var sound = document.createElement('audio');
   sound.id = 'audio-player';
   sound.controls = 'controls';
   sound.src = `${lines[key].audio}`;
   sound.type = 'audio/mpeg';
-  document.body.appendChild(sound);
+  cont.appendChild(sound);
+
+  var textEl = document.createElement('p');
+  textEl.id = 'text';
+  textNode = document.createTextNode(lines[key].text);
+  textEl.appendChild(textNode);
+  cont.appendChild(textEl);
 }
